ec92761d7b43eb789b3e19348d8720d4e25c7a6b
Amin PO Muar <helmi007@yahoo.com>
2014-04-23 18:48:10 +0800

    semak sama ada $url wujud tak

---
+++ aplikasi/papar/sesat/index.php
@@ -0,0 +1,11 @@
+<div class="container">
+<div class="hero-unit">
+<h1>Sesat daa</h1>
+<p><?php echo $this->mesej; ?></p>
+<pre>$url=><?php 
+if( isset($this->url) )
+{
+	print_r($this->url);
+}	?></pre>
+</div>
+</div>
