1ae95f6d8541a476e9fc9a22a6a047b4059e0f06
Amin PO Muar <helmi007@yahoo.com>
2014-11-04 17:12:59 +0800

    tambah fail suku1.php untuk qss

--- aplikasi/kawal/suku1.php
+++ aplikasi/kawal/suku1.php
@@ -69,6 +69,7 @@ class Suku1 extends Kawal
 			. ',concat_ws(\' \',msic2008,utama) as `msic`'
 			. ',concat_ws(\' \',alamat1,alamat2,poskod,bandar) as `alamat penuh`' 
 			. ',concat_ws(\'-\',fe,respon,catatan_jp_negeri) as keputusan'
+			. ',concat_ws(\'-\',`Untuk Perhatian Pertama Tahunan`,`Telefon Pertama Tahunan`,`Faks Pertama Tahunan`,`E-mel Pertama Tahunan`) as hubungi'
 			. '',$cari,$susun);
 		$this->papar->medanID ='newss';
 		//echo '<pre>$senaraiData->', print_r($this->papar->senaraiData, 1) . '</pre>';# papar $senaraiData
